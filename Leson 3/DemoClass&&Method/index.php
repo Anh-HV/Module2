b3e6558bfbd96dab01e1da7b5451d9c274b184d1
VietAnh.Hoang <66953864+Anh-HV@users.noreply.github.com>
2021-04-06 17:16:23 +0700

    Delete Leson 3 directory

--- Leson 3/DemoClass&&Method/index.php
+++
@@ -1,6 +0,0 @@
-<?php include("class_lib.php"); ?>
-<?php
- $stefan = new Person("Stefan Mischook");
- echo "Stefan's full name: ".$stefan->getPinnNumber();
- echo "Tell me private stuff: ".$stefan->pinnNumber;
-?>
--- Leson 3/Rectangle/index.php
+++
@@ -1,6 +0,0 @@
-<?php include("rec.php"); ?>
-<?php 
-    $rec = new Rectange(50,30);
-    echo $rec->display();
-    echo "<br />";
-    echo "S = ".$rec->getArea()." CV = ".$rec->getPerimeter();
